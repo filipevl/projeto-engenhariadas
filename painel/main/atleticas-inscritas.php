3135cb93bfb699d7e7f861cdb9f065da622ef39d
Filipe Viana <filipe.viana@ufv.br>
2020-05-03 10:39:54 -0300

    Atléticas inscritas - Botão dados cadastrais

--- painel/main/atleticas-inscritas.php
+++ painel/main/atleticas-inscritas.php
@@ -107,14 +107,110 @@
 				<!-- end: sidebar -->
 
 				<!-- Inicio da seção main(parte de exibição das paginas do painel) -->
-				<?php 
-					require 'atletic-inc.html';
-				?>				
+				<section role="main" class="content-body">
+					<header class="page-header">
+						<h2>Atléticas inscritas</h2>
+
+						<div class="right-wrapper pull-right">
+							<ol class="breadcrumbs">
+								<li>
+									<a href="index.html">
+										<i class="fa fa-home"></i>
+									</a>
+								</li>
+								<li><span>Atléticas inscritas</span></li>
+							</ol>
+
+							<a class="sidebar-right-toggle" ></a>
+						</div>
+					</header>
+					<div class="form-group">
+						<label class="col-md-3 control-label">Escolha sua instituição: <span class="required">*</span></label>
+						<div class="col-md-6">
+							<select name="instituicao" class="form-control"> // Check box com a instituição
+								<option>UFMG</option>
+
+								<option>UFV</option>
+								<!-- <?php
+									// Bucar e exibir dados da coluna instituição dica: echo <option value="x">$variável_do_banco</option>
+								?> -->
+
+							</select>
+						</div>
+					</div>
+					<div class="panel-body">
+						<a><button type="button" class="mb-xs mt-xs mr-xs btn btn-default">Dados Cadastrais</button>
+						</a>
+						<a><button type="button" class="mb-xs mt-xs mr-xs btn btn-primary">Baixar MP3</button>
+						</a>
+						<a><button type="button" class="mb-xs mt-xs mr-xs btn btn-success">Modalidades inscritas</button>
+						</a>
+						<a><button type="button" class="mb-xs mt-xs mr-xs btn btn-info">Lista geral de atletas</button>
+						</a>
+						<a><button type="button" class="mb-xs mt-xs mr-xs btn btn-warning">Lista individuais</button>
+						</a>
+						<button type="button" class="mb-xs mt-xs mr-xs btn btn-danger">Planilha da reunião</button>
+						<a>
+					</div>
+					<div>
+						<h1>Botão dados cadastrais</h1>
+						
+						<div class="panel-body">
+							<button id="default-success" class="btnCNPJ">Validar CNPJ</button>
+						</div>
+
+						<div class="panel-body">
+							<div class="table-responsive">
+								<table class="table table-striped mb-none">
+									<thead>
+										<tr>
+											<th>Presidente</th>
+											<th>Atlética</th>
+											<th>Telefone</th>
+											<th>Cidade</th>
+											<th>E-mail</th>
+											<th>CNPJ</th>
+										</tr>
+									</thead>
+									<tbody>
+										<!-- 
+										<tr>
+										o back tem que buscar os dados de cada coluna e mostrar em cada coluna	
+										</tr> -->
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</section>
+				</section>				
 			</div>
 		</section>
-		<?php 
-			require 'imports.html';
-		?>
+
+		<!-- Vendor -->
+		<script src="../assets/vendor/jquery/jquery.js"></script>
+		<script src="../assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src="../assets/vendor/bootstrap/js/bootstrap.js"></script>
+		<script src="../assets/vendor/nanoscroller/nanoscroller.js"></script>
+		<script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+		<script src="../assets/vendor/magnific-popup/magnific-popup.js"></script>
+		<script src="../assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+
+		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
+
+		<!-- Theme Base, Components and Settings -->
+		<script src="../assets/javascripts/theme.js"></script>
+
+		<!-- Theme Custom -->
+		<script src="../assets/javascripts/theme.custom.js"></script>
+
+		<!-- Theme Initialization Files -->
+		<script src="../assets/javascripts/theme.init.js"></script>
+
+		<!-- Examples -->
+		<script src="../assets/javascripts/ui-elements/examples.notifications.js"></script>
+
 	</body>
 </html>
 
